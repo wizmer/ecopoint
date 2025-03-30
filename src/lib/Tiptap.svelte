fac0022c4bfdc62ff84431e602450b1de4af1022
Benoît Coste <benoit.coste@pm.me>
2025-03-30 21:18:51 +0200

    Replace tictop by markdown editor

--- src/lib/Tiptap.svelte
+++ src/lib/Tiptap.svelte
@@ -1,98 +1,178 @@
 <script>
-	import { Editor } from '@tiptap/core';
-	import StarterKit from '@tiptap/starter-kit';
-	import { onDestroy, onMount } from 'svelte';
-	import { writable } from 'svelte/store';
-
-	import FixedMenu from './FixedMenu.svelte';
-	// @todo this throws a 'process is not defined' error in the Svelte REPL.
-	// Uncomment the next line to see the REPL issue.
-	// import BubbleMenu from '@tiptap/extension-bubble-menu'
-
+	import { marked } from 'marked';
 	export let content = '';
 
-	const contentStore = writable(content);
+	let markdown = '';
+	let fullScreen = false;
+	let preview = false;
 
-	let element;
-	let editor;
-	let bubbleMenu;
-
-	onMount(() => {
-		editor = new Editor({
-			element,
-			extensions: [StarterKit],
-			content,
-			onTransaction: () => {
-				editor = editor;
-			}
-		});
-		editor.on('update', ({ editor }) => {
-			console.log('editor html', editor.getHTML());
-			contentStore.set(editor.getHTML());
-		});
-	});
-
-	onDestroy(() => {
-		if (editor) {
-			editor.destroy();
-		}
-	});
+	// Dummy handlers for markdown editing actions
+	function handleBold() {
+		markdown += ' **bold** ';
+	}
+	function handleItalic() {
+		markdown += ' _italic_ ';
+	}
+	function handleHeading() {
+		markdown = '# Heading\n' + markdown;
+	}
+	function handleImage() {
+		markdown += ' ![alt text](url) ';
+	}
+	function handleLink() {
+		markdown += ' [link](url) ';
+	}
+	function handleEmoji() {
+		markdown += ' 😊 ';
+	}
+	function handleHashtag() {
+		markdown += ' #tag ';
+	}
+	function handleListUl() {
+		markdown += '\n- List item';
+	}
+	function handleListOl() {
+		markdown += '\n1. List item';
+	}
+	function handleCode() {
+		markdown += ' `code` ';
+	}
+	function handleQuote() {
+		markdown += '\n> Quote';
+	}
 </script>
 
-<div class="wrapper w-full border rounded-2xl">
-	<FixedMenu {editor} />
-
-	<div class="element-wrapper" bind:this={element} />
+<!-- Toolbar -->
+<div class="button-bar">
+	<div class="buttons has-addons is-pulled-right">
+		<button
+			on:click={() => window.open('https://www.camptocamp.org/articles/151910', '_blank')}
+			title="Help"
+		>
+			<i class="fa fa-question-circle" />
+		</button>
+		<span>&nbsp;</span>
+		<button
+			on:click={() => (fullScreen = !fullScreen)}
+			title={fullScreen ? 'Leave fullscreen' : 'Toggle fullscreen'}
+		>
+			<i class={'fa ' + (fullScreen ? 'fa-compress' : 'fa-expand')} />
+		</button>
+	</div>
+	<div class="buttons has-addons">
+		<button on:click={handleBold} disabled={preview} title="strong text">
+			<i class="fa fa-bold" />
+		</button>
+		<button on:click={handleItalic} disabled={preview} title="emphasized text">
+			<i class="fa fa-italic" />
+		</button>
+		<button on:click={handleHeading} disabled={preview} title="Heading">
+			<i class="fa fa-heading" />
+		</button>
+		<span>&nbsp;</span>
+		<button on:click={handleImage} disabled={preview} title="Insert image">
+			<i class="fa fa-image" />
+		</button>
+		<button on:click={handleLink} disabled={preview} title="Insert link">
+			<i class="fa fa-link" />
+		</button>
+		<button on:click={handleEmoji} disabled={preview} title="Insert emoji">
+			<i class="fa fa-grin" />
+		</button>
+		<span>&nbsp;</span>
+		<button on:click={handleHashtag} disabled={preview} title="Pitch description tag">
+			<i class="fa fa-hashtag" />
+		</button>
+		<button on:click={handleListUl} disabled={preview} title="Unordered list">
+			<i class="fa fa-list-ul" />
+		</button>
+		<button on:click={handleListOl} disabled={preview} title="Ordered list">
+			<i class="fa fa-list-ol" />
+		</button>
+		<button on:click={handleCode} disabled={preview} title="Unformatted text">
+			<i class="fa fa-code" />
+		</button>
+		<button on:click={handleQuote} disabled={preview} title="Quote">
+			<i class="fa fa-comment" />
+		</button>
+		<span>&nbsp;</span>
+		<button on:click={() => (preview = !preview)} title={preview ? 'Back to code' : 'Preview'}>
+			<i class={'fa ' + (preview ? 'fa-code' : 'fa-eye')} />
+			{preview ? ' Back to code' : ' Preview'}
+		</button>
+	</div>
 </div>
 
-<!-- {#if editor}
-	<pre class="json-output">
-	{JSON.stringify(editor.getJSON(), null, 2)}
-</pre>
+{#if fullScreen}
+	<style>
+		:global(body) {
+			overflow: hidden;
+		}
+	</style>
+{/if}
 
-	<div class="html-output">
-		{editor.getHTML()}
-	</div>
-
-	<div class="store-output">
-		{$contentStore}
-	</div>
-{/if} -->
+<h1>Markdown Editor</h1>
+<textarea bind:value={markdown} placeholder={content} class:fullscreen={fullScreen} />
+{#if !preview}
+	<div class="preview">{@html marked(markdown)}</div>
+{/if}
 
 <style>
-	.wrapper {
-		display: inline-flex;
-		flex-direction: column;
+	.button-bar {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		margin-bottom: 0.5rem;
 	}
 
-	.wrapper:focus-within {
-		border: 1px solid red;
-	}
-
-	.element-wrapper {
-		padding: 1rem;
-		flex: 1 1 0%;
-		resize: both;
-		overflow: auto;
-	}
-
-	.element-wrapper :global(p:first-of-type) {
-		margin-top: 0;
-	}
-
-	.element-wrapper :global(p:last-of-type) {
-		margin-bottom: 0;
-	}
-
-	.element-wrapper > :global(.ProseMirror) {
-		outline: 0;
-	}
-
-	.json-output,
-	.html-output {
-		max-height: 200px;
-		overflow: hidden;
-		overflow-y: auto;
+	.buttons button {
+		background: none;
 		border: 1px solid #ccc;
+		padding: 0.5rem;
+		margin-right: 0.2rem;
+		cursor: pointer;
+	}
+
+	.buttons button:disabled {
+		opacity: 0.5;
+		cursor: not-allowed;
+	}
+
+	textarea,
+	.preview {
+		box-sizing: border-box;
+		display: block;
+		width: 100%;
+	}
+
+	textarea {
+		font-family: monospace, Roboto;
+		height: 25%;
+		border: none;
+		margin: 0;
+		padding: 1rem;
+	}
+
+	.fullscreen {
+		position: fixed;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		z-index: 9999;
+		font-size: 1.2rem;
+	}
+
+	.preview {
+		height: 75%;
+		padding: 2rem;
+		border-top: solid 2px #888;
+	}
+
+	h1 {
+		background: #e1e1e1;
+		margin: 0;
+		padding: 0.8rem;
+		font-size: 1.2rem;
 	}
 </style>
--- src/routes/(main)/contribute/+page.svelte
+++ src/routes/(main)/contribute/+page.svelte
@@ -10,18 +10,18 @@
 		<form class="flex flex-col items-start gap-4">
 			<fieldset>
 				<legend class="font-semibold">Contributor Info</legend>
-				<label class="label mt-2">
+				<label class="label mt-2" for="email">
 					<span>Email</span>
-					<input class="input" type="email" placeholder="Email" />
+					<input id="email" class="input" type="email" placeholder="Email" aria-required="true" />
 				</label>
 				<div class="flex flex-row gap-2 mt-2">
-					<label class="label">
+					<label class="label" for="firstName">
 						<span>First Name</span>
-						<input class="input" type="text" placeholder="First Name" />
+						<input id="firstName" class="input" type="text" placeholder="First Name" />
 					</label>
-					<label class="label">
+					<label class="label" for="lastName">
 						<span>Last Name</span>
-						<input class="input" type="text" placeholder="Last Name" />
+						<input id="lastName" class="input" type="text" placeholder="Last Name" />
 					</label>
 				</div>
 			</fieldset>
@@ -31,7 +31,7 @@
 				<label class="label mt-2">
 					<span>Title</span>
 					<div class="flex items-center gap-2">
-						<select class="input">
+						<select id="climbingCategory" class="input" aria-label="Climbing category">
 							<option disabled selected>Climbing category</option>
 							<option value="climbing">Climbing</option>
 							<option value="bouldering">Bouldering</option>
@@ -40,9 +40,15 @@
 							<option value="multi-pitch">Multi-pitch</option>
 						</select>
 						<span class="text-nowrap">trip to</span>
-						<input class="input" type="text" placeholder="Climbing area" />
+						<input
+							id="climbingArea"
+							class="input"
+							type="text"
+							placeholder="Climbing area"
+							aria-label="Climbing area"
+						/>
 						<span>by</span>
-						<select class="input">
+						<select id="transportMethod" class="input" aria-label="Transport method">
 							<option disabled selected>Transport method</option>
 							<option value="train">Train</option>
 							<option value="bus">Bus</option>
@@ -52,53 +58,65 @@
 						</select>
 					</div>
 				</label>
-				<label class="label mt-2">
+				<label class="label mt-2" for="tripSummary">
 					<div>Summary</div>
 					<Tiptap
+						id="tripSummary"
 						content="Describe the area, or the crag, or why you like the area/crag. Just what feels right for you! Please use no more than ~100 words."
 					/>
 				</label>
-				<label class="label mt-2">
+				<label class="label mt-2" for="howToGetThere">
 					<div>How to get there</div>
 					<Tiptap
+						id="howToGetThere"
 						content="Journey from your starting point to where you stayed overnight (e.g. which train(s), durations, where to change). If you travelled by bike, provide details about bike transport and the cycle path."
 					/>
 				</label>
-				<label class="label mt-2">
+				<label class="label mt-2" for="komoot">
 					<div>Komoot</div>
 					<Tiptap
+						id="komoot"
 						content="If you have a Komoot account, you can embed your journey here. Please ensure it’s visible for everyone and in English."
 					/>
 				</label>
-				<label class="label mt-2">
+				<label class="label mt-2" for="whereToStay">
 					<div>Where to stay</div>
 					<Tiptap
+						id="whereToStay"
 						content="Add information about your accommodation (incl. website) and other recommended accommodations."
 					/>
 				</label>
-				<label class="label mt-2">
+				<label class="label mt-2" for="howToReachCrags">
 					<div>How to reach the crag(s)</div>
 					<Tiptap
+						id="howToReachCrags"
 						content="Starting from your accommodation, describe how long it takes and how to get there (bus, walking, bike, train)."
 					/>
 				</label>
-				<label class="label mt-2">
+				<label class="label mt-2" for="tips">
 					<div>Tips</div>
 					<Tiptap
+						id="tips"
 						content="Information about the nearest supermarket, restday activities (how to get there & duration), other climbing styles, nearest guidebook shop, etc."
 					/>
 				</label>
-				<label class="label mt-2">
+				<label class="label mt-2" for="howToBookTrip">
 					<div>How to book the trip</div>
-					<Tiptap content="Websites and info on how to book the public transport." />
+					<Tiptap
+						id="howToBookTrip"
+						content="Websites and info on how to book the public transport."
+					/>
 				</label>
 			</fieldset>
 
 			<fieldset>
 				<legend class="font-semibold">Author Bio</legend>
-				<label class="label mt-2">
+				<label class="label mt-2" for="aboutAuthor">
 					<div>About the author</div>
-					<Tiptap content="You can add your name and a link to your social account/website." />
+					<Tiptap
+						id="aboutAuthor"
+						content="You can add your name and a link to your social account/website."
+					/>
 				</label>
 			</fieldset>
 
@@ -110,5 +128,10 @@
 <style lang="postcss">
 	fieldset {
 		@apply border p-4 w-full;
+	}
+	/* Improve label text appearance */
+	.label span,
+	.label > div {
+		@apply font-bold text-lg text-gray-800;
 	}
 </style>
