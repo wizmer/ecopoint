9527be12cf2fb87a95265f6b1c02053ba5bde897
Benoît Coste <benoit.coste@pm.me>
2025-03-30 20:15:40 +0200

    add workflow

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
 
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
+			fallback: '404.html'
 		}),
 		paths: {
-			base: 'https://wizmer.github.io/ecopoint'
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
